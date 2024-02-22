a4adae0721769bd111c29693de05342c22760ffa
lesley chang <lesley.hanna98@gmail.com>
2024-02-21 19:51:32 -0800

    scaffold inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,52 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { Flex, Input, Select } from "@chakra-ui/react";
+import { ChangeEventHandler, useState } from "react";
+
+enum SortByType {
+  RELEVANCE = "relevance",
+  PUBLISHED_YEAR = "publishedYear",
+}
+
+const SortByOptions = {
+  Relevance: {
+    id: SortByType.RELEVANCE,
+    label: "Relevance",
+  },
+  PublishedYear: {
+    id: SortByType.PUBLISHED_YEAR,
+    label: "Published Year",
+  },
+};
 
 export default function Home() {
+  const [query, setQuery] = useState("");
+  const [sortByType, setSortByType] = useState(SortByType.RELEVANCE);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main>
+      <h1>Book Search</h1>
+      <div>
+        <Input
+          type="search"
+          placeholder="Search books"
+          value={query}
+          onChange={(event) => {
+            setQuery(event.target.value);
+          }}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <Select
+          value={sortByType}
+          onChange={(event) => {
+            setSortByType(event.target.value as SortByType);
+          }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {Object.values(SortByOptions).map((props) => (
+            <option key={props.id} value={props.id}>
+              {props.label}
+            </option>
+          ))}
+        </Select>
       </div>
     </main>
   );
---
+++ src/utils/api.ts
@@ -0,0 +1,91 @@
+import { writeFileSync } from "fs";
+
+const BASE_URL = "https://openlibrary.org/search.json?q=";
+
+export type Doc = {
+  _version_: number;
+  key: string;
+  type: string;
+  seed: string[];
+  title: string;
+  title_sort: string;
+  title_suggest: string;
+  subtitle: string;
+  edition_count: number;
+  edition_key: string[];
+  publish_date: string[];
+  publish_year: number[];
+  first_publish_year: number;
+  number_of_pages_median: number;
+  lccn: string[];
+  publish_place: string[];
+  oclc: string[];
+  contributor: string[];
+  lcc: string[];
+  isbn: string[];
+  last_modified_i: number;
+  ebook_count_i: number;
+  ebook_access: string;
+  has_fulltext: boolean;
+  public_scan_b: boolean;
+  ia: string[];
+  ia_collection: string[];
+  ia_collection_s: string;
+  printdisabled_s: string;
+  ratings_average: number;
+  ratings_sortable: number;
+  ratings_count: number;
+  ratings_count_1: number;
+  ratings_count_2: number;
+  ratings_count_3: number;
+  ratings_count_4: number;
+  ratings_count_5: number;
+  readinglog_count: number;
+  want_to_read_count: number;
+  currently_reading_count: number;
+  already_read_count: number;
+  cover_edition_key: string;
+  cover_i: number;
+  first_sentence: string[];
+  publisher: string[];
+  language: string[];
+  author_key: string[];
+  author_name: string[];
+  author_alternative_name: string[];
+  subject: string[];
+  id_goodreads: string[];
+  id_librarything: string[];
+  id_wikidata: string[];
+  ia_box_id: string[];
+  publisher_facet: string[];
+  subject_facet: string[];
+  lcc_sort: string;
+  author_facet: string[];
+  subject_key: string[];
+};
+
+export type Result = {
+  numFound: number;
+  start: number;
+  numFoundExact: true;
+  docs: Doc[];
+};
+
+export async function searchBooks(query: string): Promise<Result> {
+  const res = await fetch(BASE_URL + query, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await res.json();
+  return data;
+}
+
+// searchBooks("program")
+//   .then((data) => {
+//     writeFileSync("sample-data.json", JSON.stringify(data, null, 2));
+//     console.log(JSON.stringify(data));
+//   })
+//   .catch((error) => console.log("❌ ERROR: ", error));
